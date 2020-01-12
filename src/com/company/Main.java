edd7c44768fdd80154c20e92e792f2c0a0deee17
leslie101689 <49891074+leslie101689@users.noreply.github.com>
2020-01-12 15:42:28 +0800

    Add inner class challenge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -165,4 +165,11 @@ public class Main {
         }
         System.out.println("================================");
     }
+
+    //Modify the playlist challenge so that the Album class uses an inner class.
+    //Instead of using an ArrayList to hold its tracks, it will use an inner class called SongList
+    //The inner SongList class will use an ArrayList and will provide a method to add a song.
+    //It will also provide findSong() methods which will be used by the containing Album class
+    //to add songs to the playlist.
+    //Neither the Song classes or the Main class should be changed.
 }
